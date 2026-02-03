d8fb2835994d2cdccc6bfac1c442b8f5114752d8
ALNAFI <alnafi.eng@gmail.com>
2026-02-04 02:47:15 +0600

    main js done v2

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,5 @@
 import Footer from "../components/common/Footer";
-import ErrorContent from "../components/pages/notFound/ErrorContent";
+import ErrorContent from "../components/pages/notfound/ErrorContent";
 import Breadcrumb from "../components/common/Breadcrumb";
 import Header from "../components/common/Header";
 export default function NotFoundPage() {
